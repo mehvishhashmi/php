769328e6f0ced404ad217b128c495b797cbfc174
mehvishhashmi <mh449@njit.edu>
2017-02-04 15:14:38 -0500

    hello world new

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
 <?php
-$obj = new main();
-
-
-
-class main {
-	public function_construct() {
-	 echo 'Hello world' ;
-	}
-
-
+ 
+  $obj = new main();
+  //$car = $obj->createCar();
+  //$car->make = 'ford';
+  //$car->model = 'Mustang';
+  //$car->year = '2017';
+       
+  //print_r($car);
+	 
+	  class main {
+	    
+	  public function __construct() {
+	          echo 'hello world';
+	           }
+	        
+	//public function createCar() {
+	//$car = new car;
+	//return $car;
+	//	        }
+		     
+	//public function __destruct() {
+	//    echo 'goodbye world';
+	//	            }
+		   }
+			    
+	//class car {
+		//public $make;
+		//	         public $model;
+		//	          public $year;
+		//	         }
+			        
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->
